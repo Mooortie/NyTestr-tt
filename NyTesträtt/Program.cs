c404e149d86f734e798b21220c8e285302231fb5
Mooortie <mattias.stromon@gmail.com>
2022-08-29 15:42:44 +0200

    Ples

--- NyTesträtt/Program.cs
+++ NyTesträtt/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Vad är ditt namn?");
 string name = Console.ReadLine();
 Console.WriteLine($"Ditt namn är {name}");
+
+Console.WriteLine("Nya pushen kommer här!"); //Push no worki?
